533a976f587e27af27b64544393aadc1681bc4a6
sylvesterml <yasuno0129@gmail.com>
2018-10-21 14:33:48 +0900

    modified code

--- test_most_likely_hgpr.py
+++ test_most_likely_hgpr.py
@@ -5,8 +5,8 @@ from gpr import GaussianKernel
 from gpr import GaussianProcessRegression
 
 if __name__ == '__main__':
-    N_train = 1200
-    N_test = 200
+    N_train = 500
+    N_test = 100
     split_rate = 0.1
     N_train_1 = int(N_train * (1.0 - split_rate))
     N = N_train + N_test
@@ -53,4 +53,40 @@ if __name__ == '__main__':
         sigma_test_diag = np.diag(sigma_test_)
         sigma_test_diag.flags.writeable = True
         sigma_test_diag[np.isnan(sigma_test_diag)] = 0
-        mu_test_ave += mu_test_
+        mu_test_ave += mu_test_        
+        sigma_test_ave += sigma_test_diag
+
+    mu_test_ave /= cnt
+    sigma_test_ave /= cnt
+
+    # x_test_ = np.arange(0.0, 1.0, 1 / N_test)
+    mu_test = 2.0 * (np.exp(-30.0 * (x_test_ - 0.25) ** 2.0) + np.sin(np.pi * x_test_) ** 2) - 2.0
+    g_test = np.sin(2.0 * np.pi * x_test_)
+    t_test_ = np.random.normal(mu_test, np.exp(g_test))
+    g_test = np.sqrt(np.exp(g_test))
+
+    print(sigma_test_ave)
+
+    fig1 = plt.figure()
+    plt.fill_between(x_test_, mu_test_ave - sigma_test_ave*2, mu_test_ave - sigma_test_ave*3 \
+                     , color="c", alpha=0.2)
+    plt.fill_between(x_test_, mu_test_ave + sigma_test_ave * 2, mu_test_ave + sigma_test_ave * 3 \
+                 , color="c", alpha=0.2)
+    plt.fill_between(x_test_, mu_test_ave - sigma_test_ave, mu_test_ave - sigma_test_ave * 2 \
+                     , color="c", alpha=0.4)
+    plt.fill_between(x_test_, mu_test_ave + sigma_test_ave, mu_test_ave + sigma_test_ave * 2 \
+                     , color="c", alpha=0.4)
+    plt.fill_between(x_test_, mu_test_ave - sigma_test_ave, mu_test_ave + sigma_test_ave, color='c', alpha=0.6)
+    plt.scatter(x_test_, t_test_, color='g', s=10)
+    # plt.scatter(x_train_, t_train_, color='r')
+    plt.plot(x_test_, mu_test, color='r')
+    plt.plot(x_test_, mu_test + g_test, color='hotpink')
+    plt.plot(x_test_, mu_test - g_test, color='hotpink')
+    plt.plot(x_test_, mu_test + g_test * 2, color='violet')
+    plt.plot(x_test_, mu_test - g_test * 2, color='violet')
+    plt.plot(x_test_, mu_test + g_test * 3, color='lightpink')
+    plt.plot(x_test_, mu_test - g_test * 3, color='lightpink')
+    plt.plot(x_test_, mu_test_ave, color='b')
+    plt.ylim(-7.0, 7.0)
+    plt.savefig('test_most_likely_hgpr.png')
+    plt.show()
